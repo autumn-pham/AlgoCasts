720619ec5d383092ba46afc5ce70f2720209b805
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-11-15 12:53:48 -0800

    finished fizzbuzz and chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// MY ATTEMPT
+// function chunk(array, size) {
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     array.slice(0, size)
+//     newArray.push(newArray)
+//   }
+// }
+
+// SOLUTION 1
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let element of array) {
+//     const lastElement = chunked[chunked.length - 1];
+//     if (!lastElement || lastElement.length  === size) {
+//       chunked.push([element]);
+//     } else {
+//       lastElement.push(element)
+//     }
+//   }
+//   return chunked;
+// }
+
+// SOLUTION 2
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
