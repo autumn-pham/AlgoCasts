9aa5253656271b27dbf0bc457eecfbf45c105cad
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-11-20 10:27:54 -0800

    completed vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,33 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// MY ATTEMPT
+// function vowels(str) {
+//   let vowel = 0;
+//   for (let j = 0; j < str.length; j++) {
+//     if (j === 'a' || j === 'e' || j === 'i' || j === 'o' || j === 'a') {
+//       vowel += 1;
+//     }
+//   }
+//   return vowel;
+// }
+
+// SOLUTION 1
+// function vowels(str) {
+//   let count = 0;
+//   const vowelChecker = ['a', 'e', 'i', 'o', 'u'];
+//   for (let char of str.toLowerCase()) {
+//     if (vowelChecker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// SOLUTION 2: RegEx
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
